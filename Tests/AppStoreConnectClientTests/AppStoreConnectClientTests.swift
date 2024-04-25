c2739717634059fd8c208465e9e12e4dab4ef583
Mykhailo Bondarenko <mcmikius@icloud.com>
2024-04-25 16:27:37 +0300

    style: fix lint errors

--- Tests/AppStoreConnectClientTests/AppStoreConnectClientTests.swift
+++ Tests/AppStoreConnectClientTests/AppStoreConnectClientTests.swift
@@ -2,11 +2,5 @@ import XCTest
 @testable import AppStoreConnectClient
 
 final class AppStoreConnectClientTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
-    }
+    func testExample() throws {}
 }
