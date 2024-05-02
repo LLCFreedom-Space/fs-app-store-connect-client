91fd3329a378bbc730913d59feeced22f5347f6a
Mykola Masyk <mykola.vasyk.1@gmail.com>
2024-05-02 11:28:47 +0300

    test: add tests for JWT struct

--- Tests/AppStoreConnectClientTests/JWTTests.swift
+++ Tests/AppStoreConnectClientTests/JWTTests.swift
@@ -8,21 +8,54 @@
 import XCTest
 @testable import AppStoreConnectClient
 
-//final class JWTTests: XCTestCase {
-//    
-//    func testVerifyNotExpiredValidToken() throws {
-//        let testToken = """
-//            -----BEGIN PRIVATE KEY-----
-//            MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgX6js0tmnxknIV+w0
-//            eLfoDF2fnng+GESiHP6fmTKMMEugCgYIKoZIzj0DAQehRANCAASQpzpdWKT1n4YG
-//            i8jYKh1w/iNojELO+4RWUSZ5zceH5HdExSKWec/UOXImcpnN5alC98tcmxf7GNRe
-//            bXp8F8EX
-//            -----END PRIVATE KEY-----
-//            """
-//        let futureExpiration = Date().addingTimeInterval(1000)
-//        let payload = JWT.Payload(expirationTime: futureExpiration.timeIntervalSince1970, issuerId: UUID().uuidString, issuedAt: Date().timeIntervalSince1970)
-//        let isNotExpired = JWT.verifyNotExpired(testToken)
-//        XCTAssertTrue(isNotExpired)
-//    }
-//}
+final class JWTTests: XCTestCase {
+    
+    func testVerifyNotExpiredSuccess() throws {
+        let privateKey = """
+            -----BEGIN PRIVATE KEY-----
+            MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgX6js0tmnxknIV+w0
+            eLfoDF2fnng+GESiHP6fmTKMMEugCgYIKoZIzj0DAQehRANCAASQpzpdWKT1n4YG
+            i8jYKh1w/iNojELO+4RWUSZ5zceH5HdExSKWec/UOXImcpnN5alC98tcmxf7GNRe
+            bXp8F8EX
+            -----END PRIVATE KEY-----
+            """
+        let credentials = Credentials(
+            issuerId: UUID().uuidString,
+            keyId: UUID().uuidString,
+            privateKey: privateKey,
+            expireDuration: 2
+        )
+        let jwt = try JWT.createToken(by: credentials)
+        let result = JWT.verifyNotExpired(jwt)
+        XCTAssertTrue(result)
+    }
+    
+    func testVerifyNotExpiredFailed() throws {
+        let privateKey = """
+            -----BEGIN PRIVATE KEY-----
+            MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgX6js0tmnxknIV+w0
+            eLfoDF2fnng+GESiHP6fmTKMMEugCgYIKoZIzj0DAQehRANCAASQpzpdWKT1n4YG
+            i8jYKh1w/iNojELO+4RWUSZ5zceH5HdExSKWec/UOXImcpnN5alC98tcmxf7GNRe
+            bXp8F8EX
+            -----END PRIVATE KEY-----
+            """
+        let credentials = Credentials(
+            issuerId: UUID().uuidString,
+            keyId: UUID().uuidString,
+            privateKey: privateKey,
+            expireDuration: -1
+        )
+        let jwt = try JWT.createToken(by: credentials)
+        let result = JWT.verifyNotExpired(jwt)
+        XCTAssertFalse(result)
+    }
+    
+    func testInvalidPartCount() throws {
+        
+    }
+    
+    func testInvalidPayloadDecode() throws {
+        
+    }
+}
 
