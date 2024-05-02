28bcfc7295df096f5b87094b58a9badd7016e370
Mykola Masyk <mykola.vasyk.1@gmail.com>
2024-05-02 13:15:16 +0300

    test: add test for struct JWT

--- Tests/AppStoreConnectClientTests/JWTTests.swift
+++ Tests/AppStoreConnectClientTests/JWTTests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+import Foundation
 @testable import AppStoreConnectClient
 
 final class JWTTests: XCTestCase {
@@ -51,11 +52,48 @@ final class JWTTests: XCTestCase {
     }
     
     func testInvalidPartCount() throws {
-        
+        let privateKey = """
+            -----BEGIN PRIVATE KEY-----
+            MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgX6js0tmnxknIV+w0
+            eLfoDF2fnng+GESiHP6fmTKMMEugCgYIKoZIzj0DAQehRANCAASQpzpdWKT1n4YG
+            i8jYKh1w/iNojELO+4RWUSZ5zceH5HdExSKWec/UOXImcpnN5alC98tcmxf7GNRe
+            bXp8F8EX
+            -----END PRIVATE KEY-----
+            """
+        let credentials = Credentials(
+            issuerId: UUID().uuidString,
+            keyId: UUID().uuidString,
+            privateKey: privateKey,
+            expireDuration: 2
+        )
+        let jwt = try JWT.createToken(by: credentials)
+        var parts = jwt.components(separatedBy: ".")
+        let resultParts = parts.remove(at: 0)
+        let result = JWT.verifyNotExpired(resultParts)
+        XCTAssertFalse(result)
     }
     
     func testInvalidPayloadDecode() throws {
-        
+        let privateKey = """
+            -----BEGIN PRIVATE KEY-----
+            MIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgX6js0tmnxknIV+w0
+            eLfoDF2fnng+GESiHP6fmTKMMEugCgYIKoZIzj0DAQehRANCAASQpzpdWKT1n4YG
+            i8jYKh1w/iNojELO+4RWUSZ5zceH5HdExSKWec/UOXImcpnN5alC98tcmxf7GNRe
+            bXp8F8EX
+            -----END PRIVATE KEY-----
+            """
+        let credentials = Credentials(
+            issuerId: UUID().uuidString,
+            keyId: UUID().uuidString,
+            privateKey: privateKey,
+            expireDuration: 2
+        )
+        let jwt = try JWT.createToken(by: credentials)
+        let index = 1
+        let takenСharacter = jwt.prefix(index)
+        let resultReplacing = jwt.replacingOccurrences(of: takenСharacter, with: "*")
+        let result = JWT.verifyNotExpired(resultReplacing)
+        XCTAssertFalse(result)
     }
 }
 
